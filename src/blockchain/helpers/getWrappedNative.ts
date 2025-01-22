4b432ff8f17b28b9a111bb5a4713ff78ebb53ba1
dannpl <dancardosobr@gmail.com>
2025-01-23 01:09:10 +0800

    change getWrappedNative to support multichain

--- src/blockchain/helpers/getWrappedNative.ts
+++ src/blockchain/helpers/getWrappedNative.ts
@@ -3,9 +3,8 @@ import { ChainId } from '../constants';
 import { WETH9 } from '../constants';
 
 export function getWrappedNative(chainId: ChainId): Token {
-    const token = WETH9[chainId];
-    if (!token) {
+    if (!(chainId in WETH9)) {
         throw new Error(`Wrapped native for chainId ${chainId} not found`);
     }
-    return token;
+    return WETH9[chainId as keyof typeof WETH9];
 }
